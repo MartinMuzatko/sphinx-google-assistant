33cfc5e6d8c233c200f19dd49002aa036aebe5fd
Martin Muzatko <martin@happy-css.com>
2021-08-16 10:19:49 +0200

    add google assistant response

--- index.ts
+++ index.ts
@@ -1,8 +1,36 @@
+
+const x = {
+    session: {
+        id: 'example_session_id',
+        params: {}
+    },
+    prompt: {
+        override: false,
+        content: {
+            card: {
+                title: 'Card Title',
+                subtitle: 'Card Subtitle',
+                text: 'Card Content',
+                image: {
+                    alt: 'Google Assistant logo',
+                    height: 0,
+                    url: 'https://developers.google.com/assistant/assistant_96.png',
+                    width: 0
+                }
+            }
+        },
+        firstSimple: {
+            speech: 'This is a card rich response.',
+            text: ''
+        }
+    }
+}
+
 addEventListener('fetch', (event) => {
     event.respondWith(
-        new Response('Hello world', {
+        new Response(JSON.stringify(x), {
             status: 200,
-            headers: { 'content-type': 'text/plain' },
+            headers: { 'content-type': 'application/json' },
         }),
     );
 });
