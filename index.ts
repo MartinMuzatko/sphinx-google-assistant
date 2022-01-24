ab2743ef55290a9e1d650981e254fa7761fc4eea
Martin Muzatko <martin@happy-css.com>
2022-01-24 21:43:39 +0100

    connect

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { Client } from 'https://deno.land/x/mqtt@0.1.2/deno/mod.ts'
 const client = new Client({ url: 'mqtt://s-s-f.de' })
 
 addEventListener('fetch', async (event) => {
+    await client.connect()
     client.publish('sphinx', JSON.stringify(event))
     event.respondWith(
         new Response('ok', {
