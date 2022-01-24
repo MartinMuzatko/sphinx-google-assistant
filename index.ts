a1c7dffc961929461938f65b8c34a13264f06226
Martin Muzatko <martin@happy-css.com>
2022-01-24 21:45:56 +0100

    move client inside fetch

--- index.ts
+++ index.ts
@@ -1,8 +1,7 @@
 import { Client } from 'https://deno.land/x/mqtt@0.1.2/deno/mod.ts'
 
-const client = new Client({ url: 'mqtt://s-s-f.de' })
-
 addEventListener('fetch', async (event) => {
+    const client = new Client({ url: 'mqtt://s-s-f.de' })
     await client.connect()
     client.publish('sphinx', JSON.stringify(event))
     event.respondWith(
