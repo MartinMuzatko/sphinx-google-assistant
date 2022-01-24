b125c1279a4a4d6a42e449d6c325ee1e086a408a
Martin Muzatko <martin@happy-css.com>
2022-01-24 21:35:44 +0100

    push to ssf broker

--- index.ts
+++ index.ts
@@ -2,39 +2,12 @@ import { Client } from 'https://deno.land/x/mqtt@0.1.2/deno/mod.ts'
 
 const client = new Client({ url: 'mqtt://s-s-f.de' })
 
-const x = {
-    session: {
-        id: 'example_session_id',
-        params: {}
-    },
-    prompt: {
-        override: false,
-        content: {
-            card: {
-                title: 'Bam Oida',
-                subtitle: 'Moch i jo glott!',
-                text: 'Miau',
-                image: {
-                    alt: 'Google Assistant logo',
-                    height: 0,
-                    url: 'https://developers.google.com/assistant/assistant_96.png',
-                    width: 0
-                }
-            }
-        },
-        firstSimple: {
-            speech: 'Na von mir aus, aber ich weiß nicht wieso ich mich so gelangweilt anhöre',
-            text: ''
-        }
-    }
-}
-
 addEventListener('fetch', (event) => {
     client.publish('sphinx', JSON.stringify(event))
     event.respondWith(
-        new Response(JSON.stringify(x), {
+        new Response('ok', {
             status: 200,
             headers: { 'content-type': 'application/json' },
         }),
     );
-});
+})
